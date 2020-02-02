4fe0425c62c25f3f35ecdfde59242fb7895abe91
Dev P. Sharma <44609609+codewithdev@users.noreply.github.com>
2020-02-02 23:21:41 +0530

    Create variables.js

---
+++ Module-1/variables.js
@@ -0,0 +1,38 @@
+/*<!DOCTYPE html>
+<html>
+	<head>
+		<title>Oprah Likes You. And so do other people.</title>
+
+		<style>
+			
+			/* Basic CSS comment */	
+			body {
+				background: #BBB;
+				color: white;
+				width: 960px;
+				margin: 0 auto;
+			}
+
+			h1 {
+				font-size: 80px;
+				color: #000	;
+			}
+
+		</style>
+*/
+        <script>
+        var a=1;
+        var b=2;
+        var c= a+b;
+        	var username= window.prompt("Hey! What's Your name?");
+        	document.write("<h1> Hello " + username + "</h1>");
+          document.write("<h2> You've Enterered </h2> + c );
+        </script>
+/*	</head>
+  <body>
+	</body>
+
+</html>
+*/
+ 	
+	
