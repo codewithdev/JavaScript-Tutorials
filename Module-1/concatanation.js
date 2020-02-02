50c49ac99f6517523ba20b73eac3b74d868e0419
Dev P. Sharma <44609609+codewithdev@users.noreply.github.com>
2020-02-02 23:23:14 +0530

    Create concatanation.js

---
+++ Module-1/concatanation.js
@@ -0,0 +1,34 @@
+/* <!DOCTYPE html>
+<html>
+	<head>
+		<title>Oprah Likes You. And so do other people.</title>
+
+		<style>
+			
+			/* Basic CSS comment */	
+			body {
+				background: #BBB;
+				color: white;
+				width: 960px;
+				margin: 0 auto;
+			}
+
+			h1 {
+				font-size: 80px;
+				color: #000	;
+			}
+
+		</style>
+*/
+        <script>
+        	var username= window.prompt("Hey! What's Your name?");
+        	document.write("<h1> Hello " + username + "</h1>");
+ /* We've used Concatanation here */        	
+        	</script>
+	/*</head>
+	<body>
+		
+	</body>
+
+</html>
+*/
